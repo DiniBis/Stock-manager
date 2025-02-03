70e7a999f9d0186798fdfe70fcf42c6f7d18b799
Din <106832577+DiniBis@users.noreply.github.com>
2025-02-03 09:21:45 +0100

    Adding GUI and SQL connection

---
+++ src/main/java/local/istore/MyFrame.java
@@ -0,0 +1,23 @@
+package local.istore;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyFrame extends JFrame {
+    public MyFrame() {
+        this.setSize(300, 200);
+        // define the default operation when
+        // the frame is closed
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // define the title of the frame
+        this.setTitle("iStore");
+
+        JPanel panel = new JPanel();
+        panel.add(new JButton("Connexion"));
+        // add more elements if you like
+        this.setContentPane(panel);
+    }
+
+}
